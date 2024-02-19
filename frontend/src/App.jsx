b585d26b84d94d9f86a8727d51456d468d0ecc00
Anmol Shukla <anmol.28422@gmail.com>
2024-02-19 14:58:33 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,28 @@
+function App() {
+  return (
+    <div className="text-red-500 bg-slate-900 p-2">
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
