ab4f7c74cc748ce405d147caa89883f3c7d92227
Anmol Shukla <anmol.28422@gmail.com>
2024-02-20 16:38:31 +0530

    sidebar effect

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const SideBar = () => {
-  return <div>SideBar</div>;
+  return (
+    <aside
+      className="flex flex-col items-center min-w-12 sm:w-16 sticky top-0 left-0 h-screen py-8 overflow-y-auto border-r
+    bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 hover:bg-gray-600/10 border-gray-800 text-white
+    "
+    ></aside>
+  );
 };
 
 export default SideBar;
