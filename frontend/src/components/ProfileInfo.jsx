b25a0517a10f24835431e627c8adcaaae8568837
Anmol Shukla <anmol.28422@gmail.com>
2024-02-23 23:08:30 +0530

    fetching real data

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -7,29 +7,17 @@ import {
 import { FaXTwitter } from "react-icons/fa6";
 import { TfiThought } from "react-icons/tfi";
 import { FaEye } from "react-icons/fa";
+import { formatMemberSince } from "../utils/functions";
 
-const ProfileInfo = () => {
-  const userProfile = {
-    avatar_url:
-      "https://ps.w.org/user-avatar-reloaded/assets/icon-256x256.png?rev=2540745",
-    bio: "Final Year Engineering Student || Development & DevOps. 👨🏻‍💻👨🏻‍💻👨🏻‍💻",
-    email: "anmol.28422@gmail.com",
-    followers: 9,
-    following: 21,
-    html_url: "https://github.com/AnmolShukla2002",
-    location: "Kanpur, Uttar Pradesh",
-    name: "Anmol Shukla",
-    public_gists: 50,
-    public_repos: 28,
-    twitter_username: "bunny_782",
-    login: "AnmolShukla2002",
-  };
+const ProfileInfo = ({ userProfile }) => {
+  if (!userProfile) return null;
+
+  const memberSince = formatMemberSince(userProfile?.created_at);
 
   return (
     <div className="lg:w-1/3 w-full flex flex-col gap-2 md:sticky md:top-10">
       <div className="bg-glass rounded-lg p-4">
         <div className="flex gap-4 items-center">
-          {/* User Avatar */}
           <a href={userProfile?.html_url} target="_blank" rel="noreferrer">
             <img
               src={userProfile?.avatar_url}
@@ -37,10 +25,9 @@ const ProfileInfo = () => {
               alt=""
             />
           </a>
-          {/* View on Github */}
           <div className="flex gap-2 items-center flex-col">
             <a
-              href={userProfile.html_url}
+              href={userProfile?.html_url}
               target="_blank"
               rel="noreferrer"
               className="bg-glass font-medium w-full text-xs p-2 rounded-md cursor-pointer border border-blue-400 flex items-center gap-2"
@@ -51,24 +38,21 @@ const ProfileInfo = () => {
           </div>
         </div>
 
-        {/* User Bio */}
-        {userProfile?.bio ? (
+        {userProfile.bio && (
           <div className="flex items-center gap-2">
             <TfiThought />
-            <p className="text-sm">{userProfile?.bio.substring(0, 60)}...</p>
+            <p className="text-sm">{userProfile.bio.substring(0, 60)}...</p>
           </div>
-        ) : null}
+        )}
 
-        {/* Location */}
-        {userProfile?.location ? (
+        {userProfile.location && (
           <div className="flex items-center gap-2">
             <IoLocationOutline />
-            {userProfile?.location}
+            {userProfile.location}
           </div>
-        ) : null}
+        )}
 
-        {/* Twitter Username */}
-        {userProfile?.twitter_username ? (
+        {userProfile.twitter_username && (
           <a
             href={`https://twitter.com/${userProfile.twitter_username}`}
             target="_blank"
@@ -76,65 +60,58 @@ const ProfileInfo = () => {
             className="flex items-center gap-2 hover:text-sky-500"
           >
             <FaXTwitter />
-            {userProfile?.twitter_username}
+            {userProfile.twitter_username}
           </a>
-        ) : null}
+        )}
 
-        {/* Member Since Date */}
         <div className="my-2">
           <p className="text-gray-600 font-bold text-sm">Member since</p>
-          <p className="">21 Sep, 2021</p>
+          <p>{memberSince}</p>
         </div>
 
-        {/* Email Address */}
-        {userProfile?.email && (
+        {userProfile.email && (
           <div className="my-2">
             <p className="text-gray-600 font-bold text-sm">Email address</p>
-            <p className="">{userProfile.email}</p>
+            <p>{userProfile.email}</p>
           </div>
         )}
 
-        {/* Full Name */}
-        {userProfile?.name && (
+        {userProfile.name && (
           <div className="my-2">
             <p className="text-gray-600 font-bold text-sm">Full name</p>
-            <p className="">{userProfile?.name}</p>
+            <p>{userProfile.name}</p>
           </div>
         )}
 
-        {/* Username */}
         <div className="my-2">
           <p className="text-gray-600 font-bold text-sm">Username</p>
-          <p className="">{userProfile?.login}</p>
+          <p>{userProfile.login}</p>
         </div>
       </div>
 
       <div className="flex flex-wrap gap-2 mx-4">
-        {/* Followers Count */}
         <div className="flex items-center gap-2 bg-glass rounded-lg p-2 flex-1 min-w-24">
           <RiUserFollowFill className="w-5 h-5 text-blue-800" />
-          <p className="text-xs">Followers: {userProfile?.followers}</p>
+          <p className="text-xs">Followers: {userProfile.followers}</p>
         </div>
 
-        {/* Following count */}
         <div className="flex items-center gap-2 bg-glass rounded-lg p-2 flex-1 min-w-24">
           <RiUserFollowLine className="w-5 h-5 text-blue-800" />
-          <p className="text-xs">Following: {userProfile?.following}</p>
+          <p className="text-xs">Following: {userProfile.following}</p>
         </div>
 
-        {/* Number of public repos */}
         <div className="flex items-center gap-2 bg-glass rounded-lg p-2 flex-1 min-w-24">
           <RiGitRepositoryFill className="w-5 h-5 text-blue-800" />
-          <p className="text-xs">Public repos: {userProfile?.public_repos}</p>
+          <p className="text-xs">Public repos: {userProfile.public_repos}</p>
         </div>
 
-        {/* Number of public gists */}
         <div className="flex items-center gap-2 bg-glass rounded-lg p-2 flex-1 min-w-24">
           <RiGitRepositoryFill className="w-5 h-5 text-blue-800" />
-          <p className="text-xs">Public gists: {userProfile?.public_gists}</p>
+          <p className="text-xs">Public gists: {userProfile.public_gists}</p>
         </div>
       </div>
     </div>
   );
 };
+
 export default ProfileInfo;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,46 @@
+import { useCallback, useEffect, useState } from "react";
 import ProfileInfo from "../components/ProfileInfo";
 import Repos from "../components/Repos";
 import Search from "../components/Search";
 import SortRepos from "../components/SortRepos";
+import toast from "react-hot-toast";
 
 const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [sortType, setSortType] = useState("forks");
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(
+        "https://api.github.com/users/AnmolShukla2002"
+      );
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+
+      const repoRes = await fetch(userProfile.repos_url);
+      const repos = await repoRes.json();
+      setRepos(repos);
+      console.log("userProfile", userProfile);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
   return (
     <div className="m-4">
       <Search />
       <SortRepos />
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-        <ProfileInfo />
+        <ProfileInfo userProfile={userProfile} />
         <Repos />
       </div>
     </div>
