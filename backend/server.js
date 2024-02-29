f6b906347ff09018c18ca2b74db90b8d19bbe3c9
Anmol Shukla <anmol.28422@gmail.com>
2024-02-29 10:29:01 +0530

    send api be to fe

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import userRoute from "./routes/userRoute.js";
+import exploreRoutes from "./routes/exploreRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
 app.use("/api/users", userRoute);
+app.use("/api/explore", exploreRoutes);
 
 app.listen(5000, () => {
   console.log("Server running at port 5000.");
