eb22d03e630bb8e41bcaf429091db78334b54c14
Anmol Shukla <anmol.28422@gmail.com>
2024-03-01 22:01:02 +0530

    auth using passportJS

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
+
+import "./passport/githubAuth.js";
+
+import passport from "passport";
+import session from "express-session";
+
 import userRoute from "./routes/userRoute.js";
 import exploreRoutes from "./routes/exploreRoutes.js";
+import authRoute from "./routes/authRoute.js";
+
 import cors from "cors";
 import connectDB from "./db/connectDB.js";
 
 dotenv.config();
 const app = express();
+
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(cors());
 
@@ -14,6 +28,7 @@ app.get("/", (req, res) => {
   res.send("Hello from server");
 });
 
+app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/explore", exploreRoutes);
 
